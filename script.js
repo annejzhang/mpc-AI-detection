c12f6b1efa6127d886e7f9c5d8290c5c5b8ba0a6
Chace Wang <chacewang@Chaces-MacBook-Air.local>
2025-07-06 22:20:07 +1200

    Final local changes before Render deployment

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Constants
-const API_URL = 'http://localhost:3000';
+const API_URL = 'https://mpc-ai-detection.onrender.com/';
 
 // Wait for the DOM to be fully loaded
 document.addEventListener('DOMContentLoaded', () => {
